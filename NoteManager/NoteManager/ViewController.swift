b091a57a19fef702ac1605c4c08d82c7feabeed1
PatrickHD <phd6x2@mail.missouri.edu>
2018-04-26 16:19:12 -0500

    Added project files

---
+++ NoteManager/NoteManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoteManager
+//
+//  Created by Pat on 4/26/18.
+//  Copyright © 2018 Mizzou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
